fe46276bba8fa4cc4d21443a343938a05687930a
SarahKirksey <sarahbeatricedanielle@gmail.com>
2022-07-27 13:32:57 -0700

    Small correction to >enter [number]

--- commands/enter.js
+++ commands/enter.js
@@ -74,7 +74,7 @@ exports.run = (client, message, args) => {
       if (client.traitcall.traitCheck(client,charid,"ROCKET")[1] || client.traitcall.traitCheck(client,charid,"SPACE")[0] || client.charcall.allData(client,userid,charid,"godtier")==true) {
         msg+= `House hasn't been built high enough to reach that gate, but you don't care, you can fly!\n`
       } else {
-        message.channel.send(`House hasn't been built high enough to reach that gate! Have your client player build up your house with ${client.auth.prefix}build`);
+        message.channel.send(`House hasn't been built high enough to reach that gate! Have your server player build up your house with ${client.auth.prefix}build`);
         return;
       }
     }
