652e5fbd4f3fe99f60f43b5f48288d8fb09ec030
SarahKirksey <sarahbeatricedanielle@gmail.com>
2022-07-30 23:21:39 -0700

    Standardize whitespace in ath.js

--- commands/ath.js
+++ commands/ath.js
@@ -8,11 +8,8 @@ exports.use = `">ath" lists all items you've alchemized or added to your atheneu
 ">ath tier [number] [tier number]" changes the tier of an item in your atheneum to the specified number, for cheaper experimentation with traits.
 ">ath page [pagenumber]" changes the page of your atheneum.`
 exports.run = (client, message, args) => {
-
-
   //defining the costs to alchemize the item based on the tier
-
-    const tierCost = [0,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072]
+  const tierCost = [0,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072]
 
   //defining important variables
 
@@ -54,192 +51,190 @@ exports.run = (client, message, args) => {
       }
     }
 
-	if(client.traitcall.traitCheck(client,charid,"COMPUTER")[1])
-	{
-		quick = true;
-	}
+  if(client.traitcall.traitCheck(client,charid,"COMPUTER")[1])
+  {
+    quick = true;
+  }
 
-   if(alchemiter==true||client.traitcall.compTest(client,message,charid,room)[1]){
+  if(alchemiter==true||client.traitcall.compTest(client,message,charid,room)[1]){
 
-     if(!args[0] || args[0] == "page"){
-       let page = 0;
-       if (args[1]&&args[0] == "page") {
-         page = parseInt(args[1], 10) - 1;
-         if (isNaN(page)||page<0) {
-           message.channel.send("That is not a valid page number!");
-           return;
-         }
-       }
+    if(!args[0] || args[0] == "page"){
+      let page = 0;
+      if (args[1]&&args[0] == "page") {
+        page = parseInt(args[1], 10) - 1;
+        if (isNaN(page)||page<0) {
+          message.channel.send("That is not a valid page number!");
+          return;
+        }
+      }
 
-       async function dexCheck(){
+      async function dexCheck(){
 
-       const attachment = await client.imgcall.alchCheck(client,message,page,args,registry,[],"alchemy atheneum");
+      const attachment = await client.imgcall.alchCheck(client,message,page,args,registry,[],"alchemy atheneum");
 
-         message.channel.send({files: [attachment]});
-       }
+        message.channel.send({files: [attachment]});
+      }
 
-       dexCheck();
-       return;
-   } else if(!isNaN(parseInt(args[0], 10) - 1)){
+      dexCheck();
+      return;
+    } else if(!isNaN(parseInt(args[0], 10) - 1)){
 
-     let value = parseInt(args[0], 10) - 1;
+      let value = parseInt(args[0], 10) - 1;
 
-     if(value<0||value>=registry.length){
-       message.channel.send("That is not a valid selection!");
-     }
+      if(value<0||value>=registry.length){
+        message.channel.send("That is not a valid selection!");
+      }
 
-     async function itemInspect(){
-     const attachment = await client.imgcall.inspect(client,message,args,4,registry[value]);
+      async function itemInspect(){
+      const attachment = await client.imgcall.inspect(client,message,args,4,registry[value]);
 
-       message.channel.send({content: "Inspecting item", files: [attachment]});
-     }
-     itemInspect()
+        message.channel.send({content: "Inspecting item", files: [attachment]});
+      }
+      itemInspect()
 
-   } else{
-    switch(args[0])
-    {
-    case "delete":
-    {
+    } else{
+      switch(args[0])
+      {
+      case "delete":
+      {
 
-     if(!args[1]){
-       message.channel.send("Select an item from the atheneum to delete!");
-       return;
-     }
+        if(!args[1]){
+          message.channel.send("Select an item from the atheneum to delete!");
+          return;
+        }
 
-     let value = parseInt(args[1], 10) - 1;
+        let value = parseInt(args[1], 10) - 1;
 
-     if(isNaN(value)||value<0||value>=registry.length){
-       message.channel.send("That is not a valid selection!");
-     }
+        if(isNaN(value)||value<0||value>=registry.length){
+          message.channel.send("That is not a valid selection!");
+        }
 
-     let deleted = registry.splice(value,1);
-     message.channel.send(`Deleted the ${deleted[0][0]} from the atheneum!`);
+        let deleted = registry.splice(value,1);
+        message.channel.send(`Deleted the ${deleted[0][0]} from the atheneum!`);
 
-     client.charcall.setAnyData(client,userid,charid,registry,"registry");
-     break;
+        client.charcall.setAnyData(client,userid,charid,registry,"registry");
+        break;
 
-    }
-    case "push":
-    {
+        }
+        case "push":
+        {
 
-     if(!args[1]){
-       message.channel.send("Select an item to push to the front of your atheneum!");
-       return;
-     }
+        if(!args[1]){
+          message.channel.send("Select an item to push to the front of your atheneum!");
+          return;
+        }
 
-     let value = parseInt(args[1], 10) - 1;
+        let value = parseInt(args[1], 10) - 1;
 
-     if(isNaN(value)||value<0||value>=registry.length){
-       message.channel.send("That is not a valid selection!");
-       return;
-     }
+        if(isNaN(value)||value<0||value>=registry.length){
+          message.channel.send("That is not a valid selection!");
+          return;
+        }
 
-     let temp = registry.splice(value,1);
+        let temp = registry.splice(value,1);
 
-     registry.unshift(temp[0]);
+        registry.unshift(temp[0]);
 
-     message.channel.send(`moved the ${temp[0][0]} to the first position in the atheneum!`);
-     client.charcall.setAnyData(client,userid,charid,registry,"registry");
-     break;
+        message.channel.send(`moved the ${temp[0][0]} to the first position in the atheneum!`);
+        client.charcall.setAnyData(client,userid,charid,registry,"registry");
+        break;
 
-    }
-    case "tier":
-    {
-     if(!quick){
-       message.channel.send("You need an INSTANT ALCHEMITER for that!");
-       return;
-     }
+      }
+      case "tier":
+      {
+        if(!quick){
+          message.channel.send("You need an INSTANT ALCHEMITER for that!");
+          return;
+        }
 
-     if(!args[1]){
-       message.channel.send(`Select an item in the ATHENEUM to change the TIER of using the INSTANT ALCHEMITER'S ENLARGER. \n${client.auth.prefix}atheneum tier [item] [tier]`);
-       return;
-     }
+        if(!args[1]){
+          message.channel.send(`Select an item in the ATHENEUM to change the TIER of using the INSTANT ALCHEMITER'S ENLARGER. \n${client.auth.prefix}atheneum tier [item] [tier]`);
+          return;
+        }
 
-     if(!args[2]){
-       message.channel.send("Select a desired TIER between 1 and 16");
-       return;
-     }
+        if(!args[2]){
+          message.channel.send("Select a desired TIER between 1 and 16");
+          return;
+        }
 
-     let value = parseInt(args[1], 10) - 1;
+        let value = parseInt(args[1], 10) - 1;
 
-     if(isNaN(value)||value<0||value>=registry.length){
-       message.channel.send("That is not a valid selection!");
-       return;
-     }
+        if(isNaN(value)||value<0||value>=registry.length){
+          message.channel.send("That is not a valid selection!");
+          return;
+        }
 
-     let tier = parseInt(args[2], 10);
+        let tier = parseInt(args[2], 10);
 
-     if(isNaN(tier)||tier<1||tier>16){
-       message.channel.send("That is not a valid TIER");
-       return;
-     }
-     if(client.traitcall.itemTrait(client,registry[value],"SHITTY")||client.traitcall.itemTrait(client,registry[value],"TRICKSTER")){
-       message.channel.send(`You can't change the tier of a ${(client.traitcall.itemTrait(client,registry[value],"SHITTY")?`SHITTY`:`TRICKSTER`)} item!`);
-       return;
-     }
-     registry[value][2]=tier;
-     client.charcall.setAnyData(client,userid,charid,registry,"registry");
-     message.channel.send(`Scaled the ${registry[value][0]} to TIER ${tier}!`);
-     return;
+        if(isNaN(tier)||tier<1||tier>16){
+          message.channel.send("That is not a valid TIER");
+          return;
+        }
+        if(client.traitcall.itemTrait(client,registry[value],"SHITTY")||client.traitcall.itemTrait(client,registry[value],"TRICKSTER")){
+          message.channel.send(`You can't change the tier of a ${(client.traitcall.itemTrait(client,registry[value],"SHITTY")?`SHITTY`:`TRICKSTER`)} item!`);
+          return;
+        }
+        registry[value][2]=tier;
+        client.charcall.setAnyData(client,userid,charid,registry,"registry");
+        message.channel.send(`Scaled the ${registry[value][0]} to TIER ${tier}!`);
+        return;
 
-    }
-    case "rename":
-    {
-     if(!args[1]){
-       message.channel.send(`Select an item in the ATHENEUM to change the NAME of that item. \n${client.auth.prefix}atheneum name [item] [desired name]`);
-       return;
-     }
+      }
+      case "rename":
+      {
+        if(!args[1]){
+          message.channel.send(`Select an item in the ATHENEUM to change the NAME of that item. \n${client.auth.prefix}atheneum name [item] [desired name]`);
+          return;
+        }
 
-     if(!args[2]){
-       message.channel.send("Please enter a desired name for the item!");
-       return;
-     }
+        if(!args[2]){
+          message.channel.send("Please enter a desired name for the item!");
+          return;
+        }
 
-     let value = parseInt(args[1], 10) - 1;
+        let value = parseInt(args[1], 10) - 1;
 
-     if(isNaN(value)||value<0||value>=registry.length){
-       message.channel.send("That is not a valid selection!");
-       return;
-     }
+        if(isNaN(value)||value<0||value>=registry.length){
+          message.channel.send("That is not a valid selection!");
+          return;
+        }
 
-     let selectItem = registry[value];
+        let selectItem = registry[value];
 
-   //combine all args except for selection
-     let name = client.funcall.combineArgs(args,2);
+        //combine all args except for selection
+        let name = client.funcall.combineArgs(args,2);
 
-     let code = selectItem[1];
-     let oldName = selectItem[0];
+        let code = selectItem[1];
+        let oldName = selectItem[0];
 
-     if(code == "11111111" || code.charAt(0) == "/") {
-       message.channel.send("The name of that item cannot be changed!");
-       return;
-     }
+        if(code == "11111111" || code.charAt(0) == "/") {
+          message.channel.send("The name of that item cannot be changed!");
+          return;
+        }
 
-     if(name.length > 20) {
-       message.channel.send("That name is too long!");
-       return;
-     }
+        if(name.length > 20) {
+          message.channel.send("That name is too long!");
+          return;
+        }
 
-     registry[value][0]=name;
+        registry[value][0]=name;
 
-     client.charcall.setAnyData(client,userid,charid,registry,"registry");
+        client.charcall.setAnyData(client,userid,charid,registry,"registry");
 
-     message.channel.send(`Changed the name of the ${oldName} to ${name}`);
-     return;
-
-    }
-    default:
-    {
+        message.channel.send(`Changed the name of the ${oldName} to ${name}`);
+        return;
+      }
+      default:
+      {
         message.channel.send(`${args[0]} is not a valid subcommand.`);
         let cmd = client.commands.get("help");
         cmd.run(client,message,["ath"]);
         return;
+      }
+      }
     }
-    }
-   }
- }else{
-   message.channel.send("To see and interact with your atheneum, you need to be in a room with an ALCHEMITER or COMPUTER with SBURB installed");
-   return;
- }
-
+  }else{
+    message.channel.send("To see and interact with your atheneum, you need to be in a room with an ALCHEMITER or COMPUTER with SBURB installed");
+    return;
+  }
 }
